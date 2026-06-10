4b850a46c62728d159a107e6c3e115058d13c731
rogerio-damasio <raugusto2512@gmail.com>
2026-06-10 12:06:27 -0300

    Commit (semi)finalizado

--- src/app/footer-component/footer-component.ts
+++ src/app/footer-component/footer-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer-component',
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './footer-component.html',
   styleUrl: './footer-component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+  isFooterVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const docHeight = document.documentElement.scrollHeight;
+
+    // Se o usuário chegou ao fim da página
+    this.isFooterVisible = (scrollTop + windowHeight >= docHeight);
+  }
+}
